32307ec87a84c196f63c6c1dff2007a3c6e27f38
Dustin Frisch <fooker@lab.sh>
2022-03-23 10:21:42 +0100

    More umlauts

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/fooker/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/fooker/keymap.c
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
-    KC_ESC   , DE_1     , DE_2     , DE_3     , DE_4     , DE_5     ,                                  DE_6     , DE_7     , DE_8     , DE_9     , DE_0     , DE_MINS  ,
+    KC_ESC   , DE_1     , DE_2     , DE_3     , DE_4     , DE_5     ,                                  DE_6     , DE_7     , DE_8     , DE_9     , DE_0     , DE_QUES  ,
     KC_LGUI  , DE_Q     , DE_W     , DE_E     , DE_R     , DE_T     ,                                  DE_Z     , DE_U     , DE_I     , DE_O     , DE_P     , KC_RGUI  ,
     KC_LSFT  , DE_A     , DE_S     , DE_D     , DE_F     , DE_G     ,                                  DE_H     , DE_J     , DE_K     , DE_L     , DE_COLN  , KC_RSFT  ,
     OSL(1)   , DE_Y     , DE_X     , DE_C     , DE_V     , DE_B     , OSL(4)   ,             _______ , DE_N     , DE_M     , DE_COMM  , DE_DOT   , DE_MINS  , OSL(1)   ,
@@ -33,8 +33,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_universal(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                  KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
-    _______  , DE_EXLM  , DE_AT    , DE_LCBR  , DE_RCBR  , DE_PIPE  ,                                  DE_QUOT  , _______  , _______  , _______  , DE_ASTR  , KC_F12   ,
-    KC_CAPS  , DE_HASH  , DE_DLR   , DE_LPRN  , DE_RPRN  , DE_GRV   ,                                  DE_DQUO  , _______  , _______  , _______  , DE_SCLN  , KC_CAPS  ,
+    _______  , DE_EXLM  , DE_AT    , DE_LCBR  , DE_RCBR  , DE_PIPE  ,                                  DE_QUOT  , DE_UDIA  , _______  , DE_ODIA  , DE_ASTR  , KC_F12   ,
+    KC_CAPS  , DE_ADIA  , DE_SS    , DE_LPRN  , DE_RPRN  , DE_GRV   ,                                  DE_DQUO  , DE_DLR   , _______  , DE_HASH  , DE_SCLN  , KC_CAPS  ,
     _______  , DE_PERC  , DE_PLUS  , DE_LBRC  , DE_RBRC  , DE_TILD  , _______  ,            _______  , DE_AMPR  , DE_LABK  , DE_RABK  , _______  , DE_UNDS  , _______  ,
     _______  , _______  , DE_CIRC  , _______  , _______  , _______  , KC_DEL   ,            KC_INS   , DE_EQL   , XXXXXXX  , XXXXXXX  , XXXXXXX  , DE_BSLS  , _______
   ),
